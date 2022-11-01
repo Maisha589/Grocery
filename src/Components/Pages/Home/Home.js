961afb2830b332b94e72234850b24ffea9ebbf16
Maisha589 <maisharahman589@gmail.com>
2022-11-01 20:16:54 +0600

    recent Products

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../../Home/Hero/Hero';
+import RecentProducts from '../../Home/RecentProducts/RecentProducts';
 
 const Home = () => {
     return (
         <div className='container'>
             <Hero></Hero>
+            <RecentProducts></RecentProducts>
         </div>
     );
 };
